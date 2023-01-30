2564970fb08e397b6695c5a30e9a38a150afe02c
MarianoGuevara <marianoguevara2005@gmail.com>
2023-01-30 12:31:56 -0300

    Ejercicios 1 y 2 entrada y salida

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,10 @@
-/*
+/* 
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+
 function mostrar()
 {
-	alert("Hola mundo");
+	alert("Esto funciona de maravilla"); /* alert () sirve para ejecutar algo. Una variable, texto, lo que sea. 
+													Se ejecuta lo que está entre paréntesis, en una ventanita emergente*/
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,12 @@
 /*
-Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+	Debemos lograr tomar un nombre con 'prompt' 
+	y luego mostrarlo por 'alert' al presionar el botón  'mostrar'
+*/
+
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+	nombre = prompt ("Ingresa tu nombre")	//VALOR DE ASIGNACION = Así, una variable, nombre en este caso, obtiene el valor
+	alert (nombre)							//el prompt es una pedida de datos al usuario
 }
 
